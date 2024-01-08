19bbd1ac8d33ee229a9567c29dac62c1b068e0f9
Tiramitzu <github@tira.my.id>
2024-01-08 11:55:56 +0700

    chore(auth/Auth): move login screen to the top

--- auth/Auth.tsx
+++ auth/Auth.tsx
@@ -89,8 +89,8 @@ function Auth() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
         <Stack.Screen name="Home" component={FirstScreen} />
       </Stack.Navigator>
     </NavigationContainer>
